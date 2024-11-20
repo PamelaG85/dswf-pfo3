de163aacba0ec2d4fe7800ae44e6b527186eab6c
PamelaG85 <pamela.a.gomez@gmail.com>
2024-11-19 23:17:52 -0300

    agrego los resultados por consola y modifico la función edadCanina para mostrarla por consola y por pantalla

--- main.js
+++ main.js
@@ -23,6 +23,15 @@ document.getElementById("areaForm").addEventListener("submit", function (event) 
     document.getElementById("resultadoArea").textContent = `El área del rectángulo es: ${area}`;
 });
 
+// Mostrar resultados por consola
+console.log("EJERCICIO 1: CALCULAR EL ÁREA DE UN RECTÁNGULO");
+console.log("=========================================================");
+console.log("Solución 1: " + calcularAreaRectangulo(5, 3)); //Resultado: 15
+console.log("Solución 2: " + calcularAreaRectangulo(8, 4)); //Resuldato: 32
+console.log("Solución 3: " + calcularAreaRectangulo(2, 7)); //Resuldato: 14
+console.log("/////////////////////////////////////////////////////////");
+console.log("");
+
 /*
 COMENTARIOS: Para este ejercicio lo primero que pensé fue la fórmula del área de un rectángulo,
 luego pensé en la fórmula para calcular el área de un rectángulo (longitud * área) y así armé una función
@@ -61,6 +70,15 @@ document.getElementById("cadenaForm").addEventListener("submit", function(event)
     document.getElementById("resultadoPalabras").textContent = `La cadena contiene ${cantidadPalabras} palabra(s).`;
 });
 
+// Mostrar resultados por consola
+console.log("EJERCICIO 2: CONTAR PALABRAS EN UNA CADENA");
+console.log("=========================================================");
+console.log("Solución 1: " + contarPalabras("Probando si funciona")); //Resultado: 3
+console.log("Solución 2: " + contarPalabras("Mi gato color naranja se llama Naranjo")); //Resultado: 7
+console.log("Solución 3: " + contarPalabras("Felíz primavera")); //Resultado: 2
+console.log("////////////////////////////////////////////////////////");
+console.log("");
+
 /*
 COMENTARIOS: Para este ejercicio pensé en una función que reciba como parámetro una cadena de texto y
 con la función "split" usando el espacio como separador la convierta en un array de subcadenas, es
@@ -95,6 +113,15 @@ document.getElementById("cadenaInvertidaForm").addEventListener("submit", functi
     document.getElementById("resultadoPalabrasInvertidas").textContent = `La cadena invertida es: ${cadenaInvertida}`;
 });
 
+// Mostrar resultados por consola
+console.log("EJERCICIO 3: INVERTIR UNA CADENA");
+console.log("=========================================================");
+console.log("Solución 1: " + invertirCadena("Probando si funciona")); //Resultado: anoicnuf is odnaborP
+console.log("Solución 2: " + invertirCadena("Noviembre")); //Resultado: erbmeivoN
+console.log("Solución 3: " + invertirCadena("Hola")); //Resultado: aloH
+console.log("/////////////////////////////////////////////////////////");
+console.log("");
+
 /*
 COMENTARIOS: Para este ejercicio pensé que también sería de utilidad el método "split" que usé en el ejercicio
 anterior para armar un array con las subcadenas, luego invertirlos con "reverse" y finalmente unirlos nuevamente
@@ -110,7 +137,7 @@ También una validación para asegurarme de que el input no estuviera vacío.
 //Función
 function esPalindromo(cadena) {
     const cadenaOriginalLimpia = cadena.toLowerCase().normalize("NFD").replace(/[\W_]/g, "");
-    const cadenaInvertida = cadenaOriginalLimpia.split("").reverse().join("");
+    const cadenaInvertida = invertirCadena(cadenaOriginalLimpia);
     return cadenaOriginalLimpia === cadenaInvertida;
 }
 
@@ -136,6 +163,14 @@ document.getElementById("palindromoForm").addEventListener("submit", function(ev
     }
 });
 
+// Mostrar resultados por consola
+console.log("EJERCICIO 4: ENCONTRAR EL PALÍNDROMO");
+console.log("=========================================================");
+console.log("Solución 1: " + esPalindromo("Neuquen")); //Resultado: true
+console.log("Solución 2: " + esPalindromo("Somos o no somos")); //Resultado: true
+console.log("Solución 3: " + esPalindromo("Hola")); //Resultado: false
+console.log("/////////////////////////////////////////////////////////");
+console.log("");
 
 /*
 COMENTARIOS: Este fue el ejercicio que me a primera vista me resultó más complejo, luego recordé que habíamos
@@ -146,36 +181,71 @@ Con eso identificado, me di cuenta de que con la función del ejercicio anterior
 "reverse" y "join" tenía la parte de invertir la cadena y solo faltaba agregar la comparación entre ambas cadenas.
 
 Para resolver todo, la función esPalindromo que como argumento toma la cadena orginial del input y la "limpia"
-quitando acentos, espacios, signos y mayúsculas, luego la invierte con los métodos mencionados arriba, y luego 
-compara la cadena original limpia con la invertida.
+quitando acentos, espacios, signos y mayúsculas, luego la invierte llamando a la función invertirCadena del 
+ejercicio 3, y por último compara la cadena original limpia con la invertida.
 
 Como en las anteriores, usé un formulario y agregué una validación para asegurar que el input no estuviera vacío,
 manejar los espacios y los caracteres no admitidos.
 
-Por último con un condicional se muestra el mensaje "La cadena <> es un palíndromo" si el la variable "resultado" es true
+Por último con un condicional se muestra el mensaje "La cadena <> es un palíndromo" si la variable "resultado" es true
 y "La cadena <> no es un palíndromo" si el false.
 */
 
 /*///////////////// EJERCICIO 5 - CONVERTIR LA EDAD DE UN PERRO A AÑOS HUMANOS //////////////////*/
-//Función
-function edadCanina() {
-    const edadCanina = prompt("Ingrese la edad del perro:");
+//Función para mostrar por consola
+function edadCaninaConsola() {
+    let edadCanina
+    
+    while (true) {
+        edadCanina = prompt("Ingrese la edad del perro:");
 
-    //Validar el input
-    if (isNaN(edadCanina) || edadCanina <= 0) {
-        alert("Por favor, ingrese valores numéricos válidos y mayores a cero.");
-        return;
+        //Validar el input
+        if (isNaN(edadCanina) || edadCanina <= 0) {
+            alert("Por favor, ingrese valores numéricos válidos y mayores a cero.");
+        } else {
+            break;
+        }
     }
 
     //Calcular la edad humana
-    const edadHumana = edadCanina * 7;
+    let edadHumana = edadCanina * 7;
 
-    //Mostrar el resultado
-    alert(`Tu perro tiene ${edadHumana} años humanos 🐕🧡`);
+    // Mostrar resultados por consola
+    console.log("EJERCICIO 5: CONVERTIR LA EDAD DE UN PERRO A AÑOS HUMANOS");
+    console.log("=========================================================");
+    console.log(`Tu perro tiene ${edadHumana} años humanos 🐕🧡`);
+    console.log("////////////////////////////////////////////////////////");
+
 }
 
-//Llamamos a la función dando clic al botón
-document.getElementById("calcularEdadBtn").addEventListener("click", edadCanina);
+//Llamo a la función para ejecutar y mostrar el resultado por consola
+edadCaninaConsola();
+
+//Función para mostrar por pantalla
+function edadCaninaPantalla() {
+    let edadCanina
+    
+    while (true) {
+        edadCanina = prompt("Ingrese la edad del perro:");
+
+        //Validar el input
+        if (isNaN(edadCanina) || edadCanina <= 0) {
+            alert("Por favor, ingrese valores numéricos válidos y mayores a cero.");
+        } else {
+            break;
+        }
+    }
+
+    //Calcular la edad humana
+    let edadHumana = edadCanina * 7;
+
+    // Mostrar el resultado
+    alert(`Tu perro tiene ${edadHumana} años humanos 🐕🧡`);
+
+}
+
+//Llamamos a la función dando clic al botón para mostrar por pantalla
+document.getElementById("calcularEdadBtn").addEventListener("click", edadCaninaPantalla);
 
 /*
 COMENTARIOS: Este ejercicio al principio lo había hecho como los demás con un formulario y
@@ -186,8 +256,17 @@ del prompt multiplicado por 7 y mostrar el resultado.
 
 Ahora, para implementar esto la función solicita que se ingrese la edad del perro y almacena este
 valor en la variable "edadCanina", luego pasa por la validación donde se comprueba que sea un
-número y que sea positivo.
+número y que sea positivo.  Esto se repite con un bucle "while" hasta que se ingresen valores
+correctos, lo hice así porque al principio no tenía el bucle pero al implementar que se muestren
+los resultados por consola si no tenía esto había que recargar la página para volver a probar
+entonces me pareció mejor hacerlo con un bucle.
 
-Por último muestra el resultado con un `alert` y le agregué un emoji de perrito y un corazón porque
-todos amamos a los perritos y quedaron bonitos :)
+Me encontré con este problema porque primero había hecho todas las funciones para que se manejaran
+con los inputs, pero para cumplir la consigna y mostrar por consola, se me complicaba porque me
+lo mostraba por consola y por la pantalla o saltaba el alert y era un desastre!!!
+
+Así que decidí hacer 2 funciones con la misma lógica, solo que una muestra los resultados por
+consola y se ejecuta automáticamente al cargar la página y la otra muestra el resultado con un 
+`alert` y se ejecuta al hacer click en el botón "Calcular Edad Humana", elegí hacerlo así para cambiar
+un poco y además le agregué un emoji de perrito y un corazón porque me pareció divertido y bonito :)
 */
